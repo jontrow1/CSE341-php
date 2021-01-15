bd89f648de84cbacfd7a60fc7221f87a31397838
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 08:23:57 -0500

    trying to add sidenav

--- web/homepage.php
+++ web/homepage.php
@@ -1,17 +1,19 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippits/header.php'; ?>
 
-<div id="sidebar">
-    <ul>
-        <li onclick="bio" id="bio">Dossier</li>
-        <li onclick="statSheet" id="statSheet">Stats</li>
-        <li onclick="skillSheet" id="skillSheet">Skills</li>
-    </ul>
-</div>
+<div id="mainWrapper">
+    <div id="sidebar">
+        <ul>
+            <li onclick="bio" id="bio">Dossier</li>
+            <li onclick="statSheet" id="statSheet">Stats</li>
+            <li onclick="skillSheet" id="skillSheet">Skills</li>
+        </ul>
+    </div>
 
-<div id="viewports">    
-    <div id="bioPage">This is some bio information.</div>
-    <div id="statPage">This is some stats.</div>
-    <div id="skillPage">Here are my credentials</div>
+    <div id="viewports">    
+        <div id="bioPage">This is some bio information.</div>
+        <div id="statPage">This is some stats.</div>
+        <div id="skillPage">Here are my credentials</div>
+    </div>
 </div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippits/footer.php'; ?>
--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -2,7 +2,7 @@
 
         <footer>
             <p>&copy; Jon Trowbridge, All rights reserved.<br>
-            Color scheme chosen by a six year old. Credits: Savannah Trowbridge. | Last Updated: January 15, 2021</p>
+            Color scheme chosen by a six-year-old. Credits: Savannah Trowbridge. | Last Updated: January 15, 2021</p>
         </footer>
     </div>
     <script src="../js/functions.js"></script>
