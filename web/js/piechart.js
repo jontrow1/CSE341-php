3747aea838f46fc90ad81d179b978da3bdb2d383
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 11:10:19 -0500

    still trying to add chart in skills

--- web/js/piechart.js
+++ web/js/piechart.js
@@ -1,29 +1,54 @@
-window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer",
-	{
-		title:{
-			text: "Programming Skillset"
-		},
-		legend: {
-			maxWidth: 600,
-			itemWidth: 600
-		},
-		data: [
-		{
-			type: "pie",
-			showInLegend: false,
-			legendText: "{indexLabel}",
-			dataPoints: [
-				{ y: 20, indexLabel: "SharePoint" },
-				{ y: 5, indexLabel: "SQL" },
-				{ y: 25, indexLabel: "HTML" },
-				{ y: 20, indexLabel: "CSS"},
-				{ y: 15, indexLabel: "JavaScript" },
-				{ y: 10, indexLabel: "PHP"},
-				{ y: 5, indexLabel: "Python"}
-			]
-		}
-		]
-	});
-	chart.render();
-}
+//window.onload = function () {
+//	var chart = new CanvasJS.Chart("chartContainer",
+//	{
+//		title:{
+//			text: "Programming Skillset"
+//		},
+//		legend: {
+//			maxWidth: 600,
+//			itemWidth: 600
+//		},
+//		data: [
+//		{
+//			type: "pie",
+//			showInLegend: false,
+//			legendText: "{indexLabel}",
+//			dataPoints: [
+//				{ y: 20, indexLabel: "SharePoint" },
+//				{ y: 5, indexLabel: "SQL" },
+//				{ y: 25, indexLabel: "HTML" },
+//				{ y: 20, indexLabel: "CSS"},
+//				{ y: 15, indexLabel: "JavaScript" },
+//				{ y: 10, indexLabel: "PHP"},
+//				{ y: 5, indexLabel: "Python"}
+//			]
+//		}
+//		]
+//	});
+//	chart.render();
+//}
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Skills', 'Percentage'],
+          ['SharePoint',     20],
+          ['SQL',      5],
+          ['HTML',  30],
+          ['CSS', 20],
+          ['JavaScript',    10],
+          ['PHP', 5],
+          ['OOP',    10]
+        ]);
+
+        var options = {
+          title: 'Programming Skillset'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chartContainer'));
+
+        chart.draw(data, options);
+      }
--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -7,6 +7,7 @@
     </div>
     <script src="../js/functions.js"></script>
     <script src="../js/piechart.js"></script>
-    <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <!--<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </body>
 </html>
