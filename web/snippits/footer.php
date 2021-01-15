69981a9655bad999a72686d5f6ec479a3a3f7f2a
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 11:14:13 -0500

    still trying to add chart in skills

--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -6,9 +6,8 @@
         </footer>
     </div>
     <script src="../js/functions.js"></script>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="../js/piechart.js"></script>
-    <!--<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>-->
+    <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     
 </body>
 </html>
