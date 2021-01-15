486b3e069d092ab1733f5482ece3534f9c9aae55
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 12:00:02 -0500

    finished I think

--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -2,7 +2,7 @@
 
         <footer>
             <p>&copy; Jon Trowbridge, All rights reserved.<br>
-            Color scheme chosen by a six-year-old. Credits: Savannah Trowbridge. | Last Updated: <span id="currentDate"></span></p>
+            Color scheme chosen by a six-year-old. Credits: Savannah Trowbridge. | <span id="currentDate"></span></p>
         </footer>
     </div>
     <script src="../js/functions.js"></script>
