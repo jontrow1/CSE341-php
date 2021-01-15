1dbd6ee756dd937b0eaa85a44e6752e64689bdea
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 07:39:12 -0500

    adding some information for page/CSS

--- web/homepage.php
+++ web/homepage.php
@@ -1,7 +1,15 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippits/header.php'; ?>
 
-<h1>Jonathan Trowbridge</h1>
+<div id="sidebar">
+    <ul>
+        <li onclick="bio" id="bio">Dossier</li>
+        <li onclick="statSheet" id="statSheet">Stats</li>
+        <li onclick="skillSheet" id="skillSheet">Skills</li>
+    </ul>
+</div>
 
-<div id="sidebar"></div>
+<div id="bioPage"></div>
+<div id="statPage"></div>
+<div id="skillPage"></div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippits/footer.php'; ?>
--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -2,8 +2,7 @@
 
         <footer>
             <p>&copy; Jon Trowbridge, All rights reserved.</p>
-            <p>Images are believed to be "Fair Use." Notify the author if not and they will be removed. </p>
-            <p>Last Updated: January 15, 2021</p>
+            <p>Color scheme chosen by a six year old. Credits: Savannah Trowbridge. | Last Updated: January 15, 2021</p>
         </footer>
     </div>
     <script src="../js/functions.js"></script>
