4e88ccbf87772bba33a16eaeafc9f87369bce4ce
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 08:51:19 -0500

    making the sidebar with toggles

---
+++ web/js/functions.js
@@ -0,0 +1,41 @@
+function bio() {
+    var bio = document.getElementById("bio");
+    var stat = document.getElementById("statSheet");
+    var skill = document.getElementById("skillSheet");
+    if (bio.style.display = "none") {
+        bio.style.display = "block";
+        if (stat.style.display = "block") {
+            stat.style.display = "none";
+        } else if (skill.style.display = "block") {
+            skill.style.display = "none";
+        }
+    }
+}
+
+function statSheet() {
+    var bio = document.getElementById("bio");
+    var stat = document.getElementById("statSheet");
+    var skill = document.getElementById("skillSheet");
+    if (stat.style.display = "none") {
+        stat.style.display = "block";
+        if (bio.style.display = "block") {
+            bio.style.display = "none";
+        } else if (skill.style.display = "block") {
+            skill.style.display = "none";
+        }
+    }
+}
+
+function skillSheet() {
+    var bio = document.getElementById("bio");
+    var stat = document.getElementById("statSheet");
+    var skill = document.getElementById("skillSheet");
+    if (skill.style.display = "none") {
+        skill.style.display = "block";
+        if (stat.style.display = "block") {
+            stat.style.display = "none";
+        } else if (bio.style.display = "block") {
+            bio.style.display = "none";
+        }
+    }
+}
