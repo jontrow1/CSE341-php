eb4ba9e33f9eb4387a168b58ccc97cccf862145b
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 11:12:30 -0500

    still trying to add chart in skills

--- web/homepage.php
+++ web/homepage.php
@@ -37,7 +37,7 @@
         </div>
         <div id="statSheet">This is some stats.</div>
         <div id="skillSheet">
-            <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+            <div id="chartContainer"></div>
         </div>
     </div>
 </div>
--- web/snippits/footer.php
+++ web/snippits/footer.php
@@ -6,8 +6,9 @@
         </footer>
     </div>
     <script src="../js/functions.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="../js/piechart.js"></script>
     <!--<script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
 </body>
 </html>
