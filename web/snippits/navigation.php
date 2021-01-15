6e10b6325258d2c4a215be1dd5c9dcfd8a50e6b0
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 09:43:55 -0500

    added link into navigation'

--- web/snippits/navigation.php
+++ web/snippits/navigation.php
@@ -1,4 +1,4 @@
 <ul>
     <li><a href="/homepage.php" title="View Jon Trowbridge's CSE 341 Home page">Home</a></li>
-    <li><a href="#" title="View Jon Trowbridge's CSE 341 Project page">Projects</a></li>
+    <li><a href="/projects.php" title="View Jon Trowbridge's CSE 341 Project page">Projects</a></li>
 </ul>
