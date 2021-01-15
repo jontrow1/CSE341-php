3552b619c9f32232b8dfd25b641b975a2a668057
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 10:43:38 -0500

    added more info in dossier

--- web/homepage.php
+++ web/homepage.php
@@ -28,6 +28,11 @@
                 as that is his true passion.</p>
             <p>Jon Trowbridge loves his family dearly and would do anything for them. That is the main reason he now lives in Florida, working in a place that he does not particularly like.
                 It's also the reason why he does things like playing with barbies, growing a garden full of vegetables he doesn't like, taking care of two cats that he is allergic to, and eating gluten-free foods.</p>
+            <p>Other things to mention is Jon's enjoyment of golf and disc golf. However, these are only ever enjoyed when there is someone else to enjoy them with. 
+                He has many a times taken his daughters and son out to go golfing with him, though it can be quite expensive so it doesn't happen often. He also has found a friend to go disc golfing with
+                and therefore goes regularly to throw some discs at a cage (<i>this is free</i>).</p>
+            <p>Weekly, Jon will spend times playing Dungeons and Dragons where he pretends to be anything from a Gnome Artificer to female Drow Sorcerer. The possibilities are endless when he gets into
+                roleplaying, though his wife mocks him for playing pretend house like a child.</p>
             <p id="caption">Image credited to Savannah Trowbridge</p>
         </div>
         <div id="statSheet">This is some stats.</div>
