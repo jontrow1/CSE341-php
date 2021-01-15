e519cab0344dd0fe936a79d015e08bcca6f60635
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 07:22:33 -0500

    general CSS updates

--- web/snippits/navigation.php
+++ web/snippits/navigation.php
@@ -1,4 +1,4 @@
-<ul id="nav">
+<ul>
     <li><a href="/homepage.php" title="View Jon Trowbridge's CSE 341 Home page">Home</a></li>
     <li><a href="#" title="View Jon Trowbridge's CSE 341 Project page">Projects</a></li>
 </ul>
