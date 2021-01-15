229835b917cfbd395cbbaffb1a50f44dfc2d22bc
Jon Trowbridge <tro19008@byui.edu>
2021-01-15 09:42:26 -0500

    created projects page

--- web/snippits/header.php
+++ web/snippits/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8"/>
-    <title>Jon Trowbridge Homepage | CSE 341</title>
+    <title>Jon Trowbridge | CSE 341</title>
     <link href="/css/styles.css" type="text/css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Itim&family=PT+Sans+Caption&display=swap" rel="stylesheet">
